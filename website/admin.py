2ef97c9f43fc3b7c00d13a6500ad0b0b6516586f
Sarah Kim <jiyunie13@gmail.com>
2014-03-25 21:10:16 -0700

    Completed Officer Model
    Five Model Classes:
    1. Officer: inherits from User, has office hours, has classes taken
    2. OfficeHour: has day of week, has time of day, associated with officers
    3. BerkeleyClass: has class name, associated with officers
    4. OfficerHour: used to link Officer with OfficeHour (through for ManyToManyField)
    5. OfficerClass: used to link Officer with BerkeleyClass (through for ManyToManyField)
    
    Admin Registrations:
    1. Officer: information, can add office hours, can add classes taken
    2. OfficeHour: can add day/time
    3. BerkeleyClass: can add class name

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from website.models import Officer, OfficerHour, OfficerClass, OfficeHour, BerkeleyClass
 
 # Register your models here.
+
+class OfficeHoursInline(admin.TabularInline):
+    model = OfficerHour
+    extra = 1
+
+class ClassesTakenInline(admin.TabularInline):
+    model = OfficerClass
+    extra = 1
+
+class OfficerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Identification',      {'fields': ['username', 'first_name', 'last_name', 'year_in_school']}),
+        ('Contact Information', {'fields': ['email', 'phone_number']}),
+        ('Authentification',    {'fields': ['password']}),
+        ('Permissions',         {'fields': ['groups', 'user_permissions', 'is_staff', 'is_superuser']}),
+        ('Activity',            {'fields': ['is_active', 'last_login', 'date_joined']}),
+    ]
+    inlines = (OfficeHoursInline, ClassesTakenInline,)
+
+admin.site.register(Officer, OfficerAdmin)
+admin.site.register(OfficeHour)
+admin.site.register(BerkeleyClass)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,135 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Officer(User):
+    YEAR_IN_SCHOOL_CHOICES = (
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('FH', 'Fifth Year'),
+        ('MA', 'Masters Student'),
+        ('PH', 'Ph.D. Student'),
+    )
+    year_in_school = models.CharField(max_length=2, choices=YEAR_IN_SCHOOL_CHOICES, default='FR')
+    phone_number = models.IntegerField(max_length=10)
+    office_hours = models.ManyToManyField('OfficeHour', through='OfficerHour')
+    classes_taken = models.ManyToManyField('BerkeleyClass', through='OfficerClass')
+
+    def __str__(self):
+        return self.username
+
+class OfficerHour(models.Model):
+    office_hour = models.ForeignKey('OfficeHour')
+    officer = models.ForeignKey('Officer')
+
+class OfficerClass(models.Model):
+    berkeley_class = models.ForeignKey('BerkeleyClass')
+    officer = models.ForeignKey('Officer')
+
+class OfficeHour(models.Model):
+    DAY_OF_WEEK_CHOICES = (
+        (0, 'Sunday'),
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+        (6, 'Saturday'),
+    )
+    TIME_OF_DAY_CHOICES = (
+        (10, '10 AM'),
+        (11, '11 AM'),
+        (12, '12 PM'),
+        (13, '1 PM'),
+        (14, '2 PM'),
+        (15, '3 PM'),
+        (16, '4 PM'),
+        (17, '5 PM'),
+        (18, '6 PM'),
+        (19, '7 PM'),
+        (20, '8 PM'),
+    )
+    day_dict = dict(DAY_OF_WEEK_CHOICES)
+    time_dict = dict(TIME_OF_DAY_CHOICES)
+
+    day_of_week = models.IntegerField(max_length=1, choices=DAY_OF_WEEK_CHOICES, default=1)
+    hour = models.IntegerField(max_length=2, choices=TIME_OF_DAY_CHOICES, default=10)
+    officers = models.ManyToManyField('Officer', through='OfficerHour')
+
+    def __str__(self):
+        return self.day_dict[self.day_of_week] + " " + self.time_dict[self.hour]
+
+class BerkeleyClass(models.Model):
+    CLASS_CHOICES = (
+        (10100, 'CS 10'),
+        (10610, 'CS 61A'),
+        (10611, 'CS 61AS'),
+        (10612, 'CS 61B/L'),
+        (10613, 'CS 61C'),
+        (10700, 'CS 70'),
+        (11490, 'CS 149'),
+        (11500, 'CS 150'),
+        (11600, 'CS 160'),
+        (11610, 'CS 161'),
+        (11620, 'CS 162'),
+        (11640, 'CS 164'),
+        (11690, 'CS 169'),
+        (11700, 'CS 170'),
+        (11720, 'CS 172'),
+        (11740, 'CS 174'),
+        (11760, 'CS 176'),
+        (11840, 'CS 184'),
+        (11860, 'CS 186'),
+        (11880, 'CS 188'),
+        (11890, 'CS 189'),
+        (11945, 'CS 194-5'),
+        (11948, 'CS 194-8'),
+        (11950, 'CS 195'),
+        (20200, 'EE 20'),
+        (20240, 'EE 24'),
+        (20400, 'EE 40'),
+        (20840, 'EE 84'),
+        (21050, 'EE 105'),
+        (21170, 'EE 117'),
+        (21180, 'EE 118'),
+        (21200, 'EE 120'),
+        (21210, 'EE 121'),
+        (21230, 'EE 123'),
+        (21250, 'EE 125'),
+        (21260, 'EE 126'),
+        (21270, 'EE 127'),
+        (21280, 'EE 128'),
+        (21300, 'EE 130'),
+        (21340, 'EE 134'),
+        (21370, 'EE 137A'),
+        (21371, 'EE 137B'),
+        (21400, 'EE 140'),
+        (21410, 'EE 141'),
+        (21420, 'EE 142'),
+        (21430, 'EE 143'),
+        (21440, 'EE 144'),
+        (21451, 'EE 145B'),
+        (21470, 'EE 147'),
+        (21490, 'EE 149'),
+        (21500, 'EE 150'),
+        (21920, 'EE 192'),
+        (30010, 'Math 1A'),
+        (30011, 'Math 1B'),
+        (30530, 'Math 53'),
+        (30540, 'Math 54'),
+        (31040, 'Math 104'),
+        (31100, 'Math 110'),
+        (31130, 'Math 113'),
+        (31280, 'Math 128A'),
+        (31850, 'Math 185'),
+    )
+
+    class_dict = dict(CLASS_CHOICES)
+    class_name = models.IntegerField(max_length=5, choices=CLASS_CHOICES)
+    officers = models.ManyToManyField('Officer', through='OfficerClass')
+
+    def __str__(self):
+        return self.class_dict[self.class_name]
