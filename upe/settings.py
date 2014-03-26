c8fd9f1a23095492d6ee99d38951e7a08c7d5410
Sarah Kim <jiyunie13@gmail.com>
2014-03-25 18:04:10 -0700

    Completed Officer Model [Using SQLite]
    Five Model Classes:
    1. Officer: inherits from User, has office hours, has classes taken
    2. OfficeHour: has day of week, has time of day, associated with officers
    3. BerkeleyClass: has class name, associated with officers
    4. OfficerHour: used to link Officer with OfficeHour (through for ManyToManyField)
    5. OfficerClass: used to link Officer with BerkeleyClass (through for ManyToManyField)
    
    Admin Registrations:
    1. Officer: information, can add office hours, can add classes taken
    2. OfficeHour: can add day/time
    3. BerkeleyClass: can add class name
    
    Note: Temporarily using SQLite. Just uncomment DATABASES section of upe/settings.py to switch back to PostgreSQL.

--- upe/settings.py
+++ upe/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'website',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,14 +56,20 @@ WSGI_APPLICATION = 'upe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'website',
+#         'USER': 'marvin',
+#         'PASSWORD': 'hithere',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'website',
-        'USER': 'marvin',
-        'PASSWORD': 'hithere',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
