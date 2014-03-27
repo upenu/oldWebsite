ec670d56dfa030c4424d3c6cf5e63c492303a62a
Brian Su <briansudev@gmail.com>
2014-03-27 00:00:10 -0700

    removed Requirement changed family and committee to CharField

--- website/models.py
+++ website/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Candidate(models.Model):
     user = models.OneToOneField(User)
-    #family = models.ForeignKey(Family)
-    #committee = models.ForeignKey(Committee)
+    family = models.CharField(max_length=200)
+    committee = models.CharField(max_length=200)
 
     def get_progress(self):
         pass
@@ -13,22 +13,5 @@ class Candidate(models.Model):
     def __str__(self):
         return self.user.first_name + ' ' + self.user.last_name
 
-class Requirement(models.Model):
-    candidates = models.ManyToManyField(Candidate)
-    req_name = models.CharField(max_length=500)
 
-    def __str__(self):
-        return self.req_name
-
-class Family(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
-
-class Committee(models.Model):
-    name = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
 
