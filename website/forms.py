854363563f54af7e7eb2de833ded56a5aa18b98d
Li Yan <liyan@Li-Yans-MacBook-Pro.local>
2014-04-11 02:07:31 -0700

    website/forms.py

---
+++ website/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from website import models
+
+class CompletionForm(forms.Form):
+    requirements = forms.ModelChoiceField(queryset= models.Requirement.objects.all()) 
+    candidates = forms.ModelChoiceField(queryset = models.Candidate.objects.all())
+
+    
+       
+    
+     
