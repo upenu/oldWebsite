e1fcbb249152f59400c3666c9f73de5043c6588a
Sarah Kim <jiyunie13@gmail.com>
2014-04-13 15:48:16 -0700

    Added urls to website/urls.py instead of upe/urls.py

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'upe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^', include('website.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'website.views.index', name='index'),
+    url(r'^officehours/$', 'website.views.officehours', name='officehours'),
 )
