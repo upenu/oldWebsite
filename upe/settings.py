6b57cb218d9b6dc9893bc8e1a85ca17f32e7776a
Brian Su <briansudev@gmail.com>
2014-05-22 17:35:48 -0700

    heroku support

--- upe/settings.py
+++ upe/settings.py
@@ -91,4 +91,21 @@ USE_TZ = True
 
 AUTHENTICATION_BACKENDS = ('website.backends.CustomBackend',)
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+)
