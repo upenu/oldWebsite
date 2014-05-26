aa8554ae8b67de5fc93edd313ec7a45e34fc2cd9
Brian Su <briansudev@gmail.com>
2014-05-25 18:15:04 -0700

    uncommented officer admin stuff

--- website/admin.py
+++ website/admin.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from website.models import *
 
-# Register your models here.
 
-#class ClassesTakenInline(admin.TabularInline):
-    #model = OfficerClass
-    #extra = 1
+class ClassesTakenInline(admin.TabularInline):
+    model = OfficerClass
+    extra = 1
 
-#class OfficerAdmin(admin.ModelAdmin):
-    #fields = ('user', 'position', 'photo', 'office_hours',)
-    #inlines = (ClassesTakenInline,)
+class OfficerAdmin(admin.ModelAdmin):
+    fields = ('user', 'position', 'photo', 'office_hours',)
+    inlines = (ClassesTakenInline,)
 
 admin.site.register(UserProfile)
-#admin.site.register(OfficerProfile, OfficerAdmin)
+admin.site.register(OfficerProfile, OfficerAdmin)
 #admin.site.register(OfficeHour)
 #admin.site.register(BerkeleyClass)
 #admin.site.register(CandidateProfile)
