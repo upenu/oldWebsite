05f44f884cdae424a8355d9e8fc5033b5a02bf97
Mari Batilando <mari.batilando@gmail.com>
2014-05-26 17:45:02 -0700

    Upcoming events now returns top 3 closest events

--- website/processor.py
+++ website/processor.py
@@ -1,4 +1,10 @@
 from website.models import Event
+from datetime import date, timedelta
 
+"""
+Gets Top 3 Upcoming Events from the current date
+"""
 def populate_footer(request):
-    return {'events': Event.objects.all()}
+	curr_date = date.today()
+	end_date = curr_date + timedelta(days=90)
+	return {'events': Event.objects.filter(start_time__range=[curr_date, end_date]).order_by('start_time')[:3]}
