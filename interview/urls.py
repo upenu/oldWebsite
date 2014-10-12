db46ed1f0c966c71c49251d41e0a708a82c9a60f
Phili <phili0524@berkeley.edu>
2014-10-12 12:15:40 -0700

    modified view.py

--- interview/urls.py
+++ interview/urls.py
@@ -7,4 +7,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'interview.views.index', name='interview'),
 #    url(r'^$', 'website.views.index', name='website-index'),
+
+
+    # ex: /interview/
+    url(r'^$', interview.views.index, name='index'),
+    # ex: /interview/5/
+    url(r'^(?P<question_id>\d+)/$', interview.views.detail, name='detail'),
+    # ex: /interview/5/results/
+    url(r'^(?P<question_id>\d+)/results/$', interview.views.results, name='results'), #interiew.views.results or views.results ??
 )
--- interview/views.py
+++ interview/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.contrib.auth import login
+from django.template import Context, Template
+from django.forms import *
+from django.core.serializers.json import DjangoJSONEncoder
+import json, re
+from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.views import generic
+
+from interview.models import Question, Answer
+# def index(request):
+#     template = loader.get_template('website/interview.html')
+#     context = RequestContext(request, {})
+#     return HttpResponse(template.render(context))
 
 def index(request):
-    return HttpResponse("Hello user. You're at the interview page")
+    latest_question_list = Question.objects.all()[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'website/interview.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'website/interview/detail.html', {'question': question})]
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
+
+
+#Question.objects.all()
+
