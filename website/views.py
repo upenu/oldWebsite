911b4debe4c33ce7829efcc1acc84b5f1ae9ea7f
Mari Batilando <mari.batilando@gmail.com>
2014-10-26 17:03:32 -0700

    Finish static OH

--- website/views.py
+++ website/views.py
@@ -12,6 +12,9 @@ from users.models import *
 def index(request):
     template = loader.get_template('website/index.html')
     officers = UserProfile.objects.filter(user_type=3, approved=True)
+
+    
+
     # context = RequestContext(request, { 'officers': officers })
     # return HttpResponse(template.render(context))
-    return render(request, 'website/index.html', { "officers": officers })
+    return render(request, 'website/index.html', { 'officers': officers })
