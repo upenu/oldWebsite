61eb74546eb48b8baa111ec400d75ddcec24f4f8
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-03-26 19:35:39 -0700

    OfficeHour model detailed

--- office_hours/models.py
+++ office_hours/models.py
@@ -1,12 +1,48 @@
 from django.db import models
+from users.models import UserProfile
 
 class OfficeHour(models.Model):
-    name = models.CharField(max_length=100)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
+    user = models.ForeignKey(UserProfile)
     description = models.TextField(max_length=500, blank=True, null=True)
-    location = models.CharField(max_length=100)
-    thumbnail = models.ImageField(upload_to='event_images/thumbnails')
-    banner = models.ImageField(upload_to='event_images/banners')
+
+    DAY_OF_WEEK_CHOICES = (
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+    )
+    TIME_OF_DAY_CHOICES = (
+        (11, '11 AM'),
+        (12, '12 PM'),
+        (13, '1 PM'),
+        (14, '2 PM'),
+        (15, '3 PM'),
+        (16, '4 PM'),
+        (17, '5 PM'),
+    )
+
+    day_dict = dict(DAY_OF_WEEK_CHOICES)
+    time_dict = dict(TIME_OF_DAY_CHOICES)
+
+    day_of_week = models.IntegerField(max_length=1, choices=DAY_OF_WEEK_CHOICES, default=1)
+    hour = models.IntegerField(max_length=2, choices=TIME_OF_DAY_CHOICES, default=11)
+
     def __str__(self):
         return self.name
+
+    @property
+    def officer(self):
+        return self.user.__str__()
+
+    @property
+    def time(self):
+        return self.day_dict[self.day_of_week] + " " + self.time_dict[self.hour]
+
+
+class Misc(models.Model):
+    # officer_username = models.CharField(max_length=30,
+    #     help_text='Please enter a valid officer username as this is used for website queries.')
+
+    def __str__(self):
+        return self.name() + " " + self.officer_username
--- office_hours/views.py
+++ office_hours/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def office_hours(request):
+    template = loader.get_template("...")
+
