5381cae1f7166fde545aa8c8caf73d9cbbe58e58
Shafqat Dulal <shafqat.dulal@gmail.com>
2015-03-26 19:09:24 -0700

    Start of models

--- office_hours/models.py
+++ office_hours/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class OfficeHour(models.Model):
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=100)
+    thumbnail = models.ImageField(upload_to='event_images/thumbnails')
+    banner = models.ImageField(upload_to='event_images/banners')
+    def __str__(self):
+        return self.name
