a1c56558a79b53748a75950e6b031e9ad14afd87
Michael A Knyszek <mknyszek@gmail.com>
2015-09-13 17:04:02 -0700

    made modifications for new calendar db table

--- upe_calendar/models.py
+++ upe_calendar/models.py
@@ -3,10 +3,8 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=100)
     start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
     description = models.TextField(max_length=10000, blank=True, null=True)
     location = models.CharField(max_length=100)
-    thumbnail = models.ImageField(upload_to='event_images/thumbnails')
-    banner = models.ImageField(upload_to='event_images/banners')
+    banner = models.CharField(max_length=4096)
     def __str__(self):
         return self.name
--- website/static/website/js/office_hours.js
+++ website/static/website/js/office_hours.js
@@ -8,42 +8,43 @@ $(document).ready(function () {
 	var officerList = $('.officers-oh');
 	var tileString = '<li class=\"tile\">\r\n  <div class=\"officer-oh\">\r\n    <img class=\"image\" src=\"http:\/\/upe.berkeley.edu\/media\/profile_images\/spock.jpg\">\r\n    <div class=\"info\">\r\n      <span class=\"name\"><\/span>\r\n      <span class=\"date\"><\/span>\r\n      <ul class=\"classes-skills\">\r\n      <\/ul>\r\n    <\/div>\r\n  <\/div>\r\n<\/li>';
 	var tileTemplate = $(tileString);
-	for (var i = 0; i < officers.length; i++) {
-               	var officer = officers[i];
-               	var tile = tileTemplate.clone();
+
+	for (var i = officers.length - 1; i >= 0; i--) {
+        var officer = officers[i];
+       	var tile = tileTemplate.clone();
 		var courses = '';
 		var hours = '';
 
-               	for (var j = 0; j < officer.courses.length; j++) {
+        for (var j = 0; j < officer.courses.length; j++) {
 			var times = officer.times;
-                       	var course = officer.courses[j];
+            var course = officer.courses[j];
 
 			tile.addClass(course);
-                       	courses += course.replace('_', ' ');
+            courses += course.replace('_', ' ');
 
 			if (j < officer.courses.length - 1) {
 				courses += ', ';
 			}
-               	}
+        }
 
 		for (var k = 0; k < times.length; k++) {
-                        var time = times[k];
+            var time = times[k];
 
-                        tile.addClass(time.day);
-                        hours += time.day + ' ' + time.time;
+            tile.addClass(time.day);
+            hours += time.day + ' ' + time.time;
 
-                        if (k < times.length - 1) {
-                                hours += ', ';
-                	}
-                }
+            if (k < times.length - 1) {
+                hours += ', ';
+        	}
+        }
 		
-                tile.find('.name').html(officer.name); 
+        tile.find('.name').html(officer.name); 
 		tile.find('.classes-skills').html(courses);
 		tile.find('.date').html(hours);
 
-                officerList.isotope('insert', tile);
+        officerList.isotope('insert', tile);
 
-        }
+    }
 
 	var classFilter = '',
 		dayFilter = '';
