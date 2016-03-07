cdd9e7adae9c3a1f87e342aa2d4fee5e1419cc09
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2016-03-06 18:32:36 -0800

    Add MySQL option to databases

--- upe/settings.py
+++ upe/settings.py
@@ -60,12 +60,22 @@ WSGI_APPLICATION = 'upe.wsgi.application'
 
 DATABASES = {
     'default': {
+    #'postgres': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'upe_db',
         'USER': 'admin',
         'PASSWORD': 'littlewhale',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+    },
+    #'default': {
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'upe_db',
+        'USER': 'admin',
+        'PASSWORD': 'littlewhale',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
