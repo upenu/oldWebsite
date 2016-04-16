4d6b60cdb785df0b4076ab04c6822b0ede7dee99
Takeshi Mochida <tmochida@ocf.berkeley.edu>
2016-04-16 01:12:38 -0700

    Remove files created from django's staticfiles app

--- static/website/js/landingpage_banner_custom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  var detectViewPort = function() {
-    var viewPortWidth = $(window).width();
-    $('#banner-wrapper').css("width","100%");
-  };
-  $(function(){ detectViewPort(); });
-  $(window).resize(function () { detectViewPort(); });
-});
