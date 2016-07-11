a148562cb70ab8af36d8a903e5889e5108049579
Upsilon Pi Epsilon <sang.bri@gmail.com>
2016-07-10 19:58:52 -0700

    calendar gone

--- upe_calendar/urls.py
+++ upe_calendar/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(''
     # Examples:
     # url(r'^$', 'upe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
