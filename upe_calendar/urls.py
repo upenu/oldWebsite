a037c6878160f90a2f58fae7aed6c02cfd7ca5d2
Brian Sang <sang.bri@gmail.com>
2016-07-10 18:34:46 -0700

    remove calendar again oops

--- upe_calendar/urls.py
+++ upe_calendar/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'upe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'upe_calendar.views.calendar', name='calendar'),
-    url(r'^get_calendar_info/$', 'upe_calendar.views.get_calendar_info', name='get_calendar_info'),
+    # url(r'^$', 'upe_calendar.views.calendar', name='calendar'),
+    # url(r'^get_calendar_info/$', 'upe_calendar.views.get_calendar_info', name='get_calendar_info'),
 )
