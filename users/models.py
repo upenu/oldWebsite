e30fbb5433e1b4618e8ac051e7174a96a6afd152
Isabel Zhang <isabelzhang@berkeley.edu>
2016-11-11 17:46:51 -0800

    edited models.py boolean

--- users/models.py
+++ users/models.py
@@ -38,6 +38,7 @@ class UserProfile(models.Model):
     candidate_profile = models.ForeignKey('CandidateProfile', blank=True, null=True)
     officer_profile = models.ForeignKey('OfficerProfile', blank=True, null=True)
     can_give_office_hours = models.BooleanField(initial=False)
+    
 
 
     def __str__(self):
