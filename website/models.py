477c6167029cca8bdc1984a91d40a06ba421f503
Nick Impicciche <vagrant@upeweb.local>
2016-12-04 17:47:29 +0000

    added social media feed with instagram and tswift

--- website/models.py
+++ website/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+import datetime
 
+class Post(models.Model):
+	POST_TYPES = (
+		(1, 'Instagram'),
+		(2, 'Facebook'),
+		(3, 'Twitter')
+		)
+
+	text = models.CharField(max_length=1001, default='default post text')
+	url = models.CharField(max_length=250, default='https://twitter.com/berkeleyUPE')
+	date = models.DateField()
+	#img_video = models.ImageField(default='static/website/images/icons/twitter.png')
+	img_video = models.ImageField(default='static/website/images/icons/post1.jpg')
+	media = models.ImageField(default='static/website/images/icons/instagram_logo.png')
+	likes = models.CharField(max_length=1001, default='420')
