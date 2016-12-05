c16516f8555ffc7197b0f532db519540b8e34988
vagrant <vagrant@precise32.(none)>
2016-12-05 00:42:20 +0000

    full changes

--- website/views.py
+++ website/views.py
@@ -8,7 +8,8 @@ from django.forms import *
 from django.core.serializers.json import DjangoJSONEncoder
 import json, re
 from users.models import *
-
+from upe_calendar.models import *
+from django.db.models import Q
 def index(request):
     template = loader.get_template('website/index.html')
     officers = UserProfile.objects.filter(user_type=3, approved=True)
@@ -24,3 +25,8 @@ def oh(request):
 
 def ir(request):
 	return render(request, 'website/ir.html', {})
+
+def events(request):
+    today = datetime.datetime.today()
+    events = Event.objects.filter(Q(start_time__lte = today)|Q(start_time = None))
+    return render(request, 'website/events.html',{'events':events})
